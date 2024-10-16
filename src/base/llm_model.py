4d0b36b3f4c97f612386bf354f9df9d917413b1b
Danny <dothaidan103@gmail.com>
2024-10-16 18:09:31 +0700

    build the prompt #1

--- src/base/llm_model.py
+++ src/base/llm_model.py
@@ -9,3 +9,14 @@ def get_openai_llm(model_name: str = "gpt-4o-mini",
                      temperature=temp,
                      max_retries = 2)
     return llm
+from langchain_openai import ChatOpenAI
+
+def get_openai_llm(model_name: str = "gpt-4o-mini",
+                   max_tokens = 512,
+                   temp = 0.3):
+    llm = ChatOpenAI(api_key="",
+                     model_name=model_name,
+                     max_tokens=max_tokens,
+                     temperature=temp,
+                     max_retries = 2)
+    return llm
